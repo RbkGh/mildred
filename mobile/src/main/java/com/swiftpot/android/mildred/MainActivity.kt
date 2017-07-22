31e7cd8a0d15856eb525b2e58f5ba583ccaf0a90
Ace Rbk <rbk.unlimited@gmail.com>
2017-07-22 16:02:54 +0000

    Added SignIn and Signout pngs

--- mobile/src/main/java/com/swiftpot/android/mildred/MainActivity.kt
+++ mobile/src/main/java/com/swiftpot/android/mildred/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.swiftpot.android.mildred
 
-import android.support.v7.app.AppCompatActivity
-import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+import android.net.Uri
+import android.os.Bundle
+import android.support.v4.app.FragmentTransaction
+import android.support.v7.app.AppCompatActivity
+import com.swiftpot.android.mildred.fragments.FragmentHome
+
+class MainActivity : AppCompatActivity(), FragmentHome.OnFragmentInteractionListener {
+    override fun onFragmentInteraction(uri: Uri) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tx:FragmentTransaction = supportFragmentManager.beginTransaction()
+        val fragmentHome:FragmentHome
+        //fragmentHome.
+        tx.add(R.id.homeLayout, FragmentHome.newInstance("",""))
+        tx.commit()
     }
+
+
 }
---
+++ mobile/src/main/java/com/swiftpot/android/mildred/model/HomeData.kt
@@ -0,0 +1,11 @@
+package com.swiftpot.android.mildred.model
+
+import android.databinding.BaseObservable
+
+/**
+ * Created by ace on 22/07/2017.
+ */
+
+class HomeData : BaseObservable(){
+
+}
